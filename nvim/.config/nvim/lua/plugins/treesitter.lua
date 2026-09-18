f54cc4f0b2912cac76cfb150dbc160e18c6d9208
absintheg <289622555+absintheg@users.noreply.github.com>
2026-09-18 17:50:59 +0300

    nvim: enable treesitter for buffers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -15,5 +15,15 @@ return {
 			"cpp",
 			"gitignore"
 		})
+		vim.api.nvim_create_autocmd('FileType', {
+			callback = function(ev)
+				local lang = vim.treesitter.language.get_lang(ev.match)
+				local available = require('nvim-treesitter').get_installed()
+				if vim.tbl_contains(available, lang) then
+					vim.treesitter.start()
+				end
+			end,
+		})
+
 	end
 }
