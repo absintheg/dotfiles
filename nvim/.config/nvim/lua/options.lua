0b2f5fe0c00dd0419c00f5ec05ff0e85344a675c
absintheg <289622555+absintheg@users.noreply.github.com>
2026-09-24 19:34:18 +0300

    nvim: add neovide config

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -23,6 +23,21 @@ vim.wo.wrap = false
 vim.g.maplocalleader = "\\"
 vim.g.mapleader = " "
 
+if vim.g.neovide then
+    -- Put anything you want to happen only in Neovide here
+	vim.o.guifont = "JetBrainsMonoNL Nerd Font:h11"
+	vim.opt.linespace = -2
+
+	vim.g.neovide_title_text_color = "pink"
+	vim.g.neovide_title_background_color = string.format(
+		"%x",
+		vim.api.nvim_get_hl(0, {id=vim.api.nvim_get_hl_id_by_name("Normal")}).bg
+	)
+
+	vim.g.neovide_theme = 'auto'
+	vim.g.neovide_cursor_animation_length = 0.02
+end
+
 local success = pcall(vim.cmd, "colorscheme " .. DEFAULT_COLOR_SCHEME)
 if not success then
 	print("Couldn't find the target colorscheme")
